343d61211763b8b002af20e20bf9252669d9722a
IorYohanna <yorakotosandratana@gmail.com>
2026-07-15 09:36:21 +0300

    Initialize Back

---
+++ Backend/src/main/java/com/example/leafsense/LeafsenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.leafsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafsenseApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/leafsense/LeafsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leafsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
