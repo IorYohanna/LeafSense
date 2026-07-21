6e1ab976c38ac726939c9d8e4fe0a8e59859d11d
Maer32 <tianamah32@gmail.com>
2026-07-21 19:40:04 +0300

    Metro

---
+++ Frontend/metro.config.js
@@ -0,0 +1,8 @@
+// Frontend/metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push('tflite', 'txt');
+
+module.exports = config;
