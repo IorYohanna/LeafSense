92b67c8dbb396b457cb80d4f27b25333a88db710
IorYohanna <yorakotosandratana@gmail.com>
2026-08-16 08:44:10 +0300

    update api

--- Frontend/src/constants/api.ts
+++ Frontend/src/constants/api.ts
@@ -1,2 +1,2 @@
 // Remplacez par l'IP locale de vot192.168.1.XXre PC sur le Wi-Fi (pas "localhost")
-export const API_URL = 'http://192.168.1.137:8080';
+export const API_URL = 'http://192.168.88.215:8080';
